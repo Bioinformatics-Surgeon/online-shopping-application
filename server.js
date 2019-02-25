d3445d1cc8721a07e10a65be37654f790dfe4b4d
turner1093 <46427183+turner1093@users.noreply.github.com>
2019-02-24 20:41:54 -0500

    fixed port issues

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-const PORT = process.env.PORT || "3306";
+const PORT = process.env.PORT || "3000";
 
 
 
