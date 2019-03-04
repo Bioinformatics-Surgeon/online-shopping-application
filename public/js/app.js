8798b231ecb5c4273059486789504d5ea1dd02f8
turner1093 <46427183+turner1093@users.noreply.github.com>
2019-03-03 23:36:48 -0500

    finish

--- public/js/app.js
+++ public/js/app.js
@@ -1,35 +1,59 @@
+function validateQuantity(requested, actual){
+    return requested <= actual;
+}
+
+function submitForm(e, buttonElement){
+    let form = buttonElement.form;
+    let requested = form[0].value;
+    let actual = form[2].value;
+    let productName = form[1].value;
+    
+    let shouldSendForm = validateQuantity(requested, actual);
+    console.log(productName + ': Quantity ' + requested + ' is less than ' + actual + ': ' + shouldSendForm)
+    
+    if(shouldSendForm)
+        form.submit();
+    else    
+        alert("Invalid quantity!");
+}
+
 $(function () {
 
-    // $('#test').on('click', function () {
-    //     $.get('/api/products', function (data) {
-    //         console.log(data, "items we are getting back")
-    //         $('#display').empty();
-    //         data.forEach(ele => {
-    //             $('#display').append(`<div>${ele.name}</div>`)
-    //         })
-    //     })
-    // })
-
-
-    // $("#add").on('click', function () {
-    //     const name = $('#itemInput').val()
-
-
-    //     $.post('/items', { name: name }, function (res) {
-    //         console.log(res)
-    //     })
-    // })
+    
 
     const render = function (data) {
+        
+        let newContent = data.map(function(row){
+            return `
+            <tr>
+                <td>
+                    ${row.product_name} 
+                </td>
+                <td>
+                    ${row.price}
+                </td>
+                <td>
+                    ${row.stock_quantity}
+                </td>
+                <td>
+                    <form action="/api/products" method="POST">
+                        <input type="number" id="quantity" name="quantity" />
+                        <input name="product_name" type="hidden" value="${row.product_name}" />
+                        <input name="stock_quantity" type="hidden" value="${row.stock_quantity}" />
+                        <input type="button" onclick="submitForm(event, this)" value="Add to Cart!" />
+                    </form>
+                </td>
+            </tr>
+            `
+        }).join('')
 
-        $('#myTable tr:last').after('<tr>...</tr><tr>...</tr>');
+        $('#products-table tr:last').after(newContent);
 
         console.log(data)
         // data.forEach(function (data) {
 
         // })insert into a table using jquery
     }
-
 
     const getProducts = function () {
         $.get('/api/products').then(render)
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -7,6 +7,7 @@ module.exports = function (app) {
   // GET route for getting all of the products
   app.get('/api/products', function (req, res) {
     db.Product.findAll({}).then(function (rows) {
+      res.header('Access-Control-Allow-Origin', '*');
       res.json(rows);
     }).catch(function (error) {
       res.json({ error: error });
@@ -20,21 +21,43 @@ module.exports = function (app) {
         id: req.params.id
       }
     }).then(function (data) {
+      res.header('Access-Control-Allow-Origin', '*');
       res.json(data);
     }).catch(function (error) {
+      res.header('Access-Control-Allow-Origin', '*');
       res.json({ error: error });
     });
   });
 
   // POST route for saving a new product
-  app.put('/api/products/:id', function (req, res) {
-    db.Product.update(
-      req.body, { where: { id: req.params.id } }
-    ).then(function (data) {
-      res.json(data);
+  app.post('/api/products', function (req, res) {
+    console.log(req.body)
+    let productName = req.body.product_name;
+    let quantity = req.body.quantity;
+    
+    db.Product.find({
+      where: {
+        product_name: productName
+      }
+    }).then(function(targetProduct){
+      
+      return targetProduct.stock_quantity - quantity;
+      
+    }).then(function(updatedQuantity){
+      
+      return db.Product.update(
+        {stock_quantity: updatedQuantity}, 
+        { where: { product_name: productName } 
+        
+    }).then(function (data) {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.redirect('/');
     }).catch(function (error) {
+      res.header('Access-Control-Allow-Origin', '*');
       res.json({ error: error });
     });
+    })
+    
+    
   });
-
 };
