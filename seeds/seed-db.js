44c5c216173d07072aeb3e2a65cf2992aff53b95
HTwa <turner1093@gmail.com>
2019-03-14 13:02:24 -0400

    minor changes

--- seeds/seed-db.js
+++ seeds/seed-db.js
@@ -1,61 +1,61 @@
 const db = require('../models');
 const items = [
  {
-   product_name: "Uncharted 4",
-   department_name: "Video Games",
+   product_name: "Coffee Beans",
+   department_name: "",
    price: 49.95,
    stock_quantity: 150
    },
  {
-   product_name: "DOOM",
+   product_name: "Generic Video Game",
    department_name: "Video Games",
    price: 59.99,
    stock_quantity: 200
    },
  {
-   product_name: "Crate of Spam",
+   product_name: "Bottle of Red Wine",
    department_name: "Food and Drink",
    price: 24.50,
    stock_quantity: 50
    },
  {
-   product_name: "Cool Shades",
+   product_name: "Sunglasses",
    department_name: "Apparel",
    price: 75.00,
    stock_quantity: 5
    },
  {
-   product_name: "Worn Denim Jeans",
+   product_name: "Designer Blue Jeans",
    department_name: "Apparel",
    price: 54.25,
    stock_quantity: 35
    },
  {
-   product_name: "Survival Towel",
+   product_name: "Survival Kit",
    department_name: "Necessities",
    price: 42.42,
    stock_quantity: 42
    },
  {
-   product_name: "Bill and Ted's Excellent Adventure",
+   product_name: "Some Movie",
    department_name: "Films",
    price: 15.00,
    stock_quantity: 25
    },
  {
-   product_name: "Mad Max: Fury Road",
+   product_name: "Some Book",
    department_name: "Films",
    price: 25.50,
    stock_quantity: 57
    },
  {
-   product_name: "Monopoly",
+   product_name: "Some Game",
    department_name: "Board Games",
    price: 30.50,
    stock_quantity: 35
    },
  {
-   product_name: "Yahtzee",
+   product_name: "Some Other Game",
    department_name: "Board Games",
    price: 19.95,
    stock_quantity: 23
